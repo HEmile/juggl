34a62b8333fb66d318a48a4ece3bda036ce4bc33
Emile <e.van.krieken@vu.nl>
2021-02-12 10:58:38 +0100

    Renamed source folder

--- smdc/format/__init__.py
+++
@@ -1,5 +0,0 @@
-from .format import Format
-from .typed_list import TypedList
-from .cypher import Cypher
-from .neo4j import Neo4j
-FORMAT_TYPES = {"typed_list": TypedList(), "cypher": Cypher(), "neo4j": Neo4j()}
