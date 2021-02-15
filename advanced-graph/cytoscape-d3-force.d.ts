a96e02851795c8c3399d1ba27fa1a02d414c5734
Emile <e.van.krieken@vu.nl>
2021-02-15 10:28:36 +0100

    Module declarations in single declarations file

--- advanced-graph/cytoscape-d3-force.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'cytoscape-d3-force';
---
+++ advanced-graph/declarations.d.ts
@@ -0,0 +1,7 @@
+declare module 'cytoscape-popper';
+declare module 'cytoscape-navigator';
+declare module 'cytoscape-dagre';
+declare module 'cytoscape-d3-force';
+declare module 'cytoscape-cola';
+declare module 'cytoscape-avsdf';
+// declare module 'cytoscape-cxtmenu'
