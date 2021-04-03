e4abb588175ede0ac41f54fed16b1c090f2d0725
Emile <e.van.krieken@vu.nl>
2021-04-03 16:20:08 +0200

    Prefer custom stylesheet above global groups

--- juggl/viz/stylesheet.ts
+++ juggl/viz/stylesheet.ts
@@ -105,7 +105,7 @@ export class GraphStyleSheet {
       // TODO: Ordering: If people specify some new YAML property to take into account, style groups will override this!
       const globalGroups = this.styleGroupsToSheet(this.plugin.settings.globalStyleGroups, 'global');
       const localGroups = this.styleGroupsToSheet(viz.settings.styleGroups, 'local');
-      const sheet = this.defaultSheet + customSheet + globalGroups + localGroups + this.yamlModifySheet;
+      const sheet = this.defaultSheet + globalGroups + customSheet + localGroups + this.yamlModifySheet;
       console.log(sheet);
       return sheet;
     }
