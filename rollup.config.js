1e7d9fc7de776fa77dfe336d0c0d72cb8e7b0edc
Emile <e.van.krieken@vu.nl>
2022-02-04 16:41:54 +0100

    Minify + remove sourcemaps on build

--- rollup.config.js
+++ rollup.config.js
@@ -6,37 +6,46 @@ import copy from 'rollup-plugin-copy';
 import autoPreprocess from 'svelte-preprocess';
 import ignore from 'rollup-plugin-ignore';
 import json from '@rollup/plugin-json';
+import {env} from 'process';
+import {terser} from "rollup-plugin-terser";
 
+const plugins = [
+  ignore(["path", "url"], { commonjsBugFix: true }),
+  commonjs({
+    include: ['node_modules/**', '../cytoscape.js-cxtmenu/**'],
+  }),
+  json(),
+  svelte({
+    emitCss: false,
+    preprocess: autoPreprocess(),
+  }),
+  typescript({sourceMap: false}),
+  nodeResolve({browser: true,
+    dedupe: ['svelte']})]
+
+const DEV = env.npm_lifecycle_event === "dev";
+
+if (!DEV) {
+  plugins.push(terser());
+}
+
+plugins.push(copy({
+  targets: [
+    {src: 'main.js', dest: 'docs/.obsidian/plugins/juggl'},
+    {src: 'styles.css', dest: 'docs/.obsidian/plugins/juggl'},
+  ],
+  hook: 'writeBundle',
+}));
 
 export default {
   input: 'src/main.ts',
   output: {
     dir: '.',
-    sourcemap: 'inline',
+    sourcemap: DEV ? 'inline' : false,
     format: 'cjs',
     exports: 'default',
     // banner: '/* This file is bundled with rollup. For the source code, see Github */',
   },
   external: ['obsidian'],
-  plugins: [
-    ignore(["path", "url"], { commonjsBugFix: true }),
-    commonjs({
-      include: ['node_modules/**', '../cytoscape.js-cxtmenu/**'],
-    }),
-    json(),
-    svelte({
-      emitCss: false,
-      preprocess: autoPreprocess(),
-    }),
-    typescript({sourceMap: true}),
-    nodeResolve({browser: true,
-      dedupe: ['svelte']}),
-    copy({
-      targets: [
-        {src: 'main.js', dest: 'docs/.obsidian/plugins/juggl'},
-        {src: 'styles.css', dest: 'docs/.obsidian/plugins/juggl'},
-      ],
-      hook: 'writeBundle',
-    }),
-  ],
+  plugins: plugins,
 };
